206588608010025de96e685adf1a99f2ee42af88
Canfrère Nicolas <nico.canfrere@gmail.com>
2014-03-07 18:34:05 +0100

    listeners are functionals. adapt for loops to be 1 based (not zero based) it is important ?

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -8,12 +8,18 @@
     [].slice.call(drags).forEach(function(el, idx){
         /////////////////////////////////////////////
         el.addEventListener("dragstart", function(evt){
-
+            //this.classList.add("dragstart");
+            dragSrcElm = this;
+            evt.dataTransfer.effectAllowed = "copy";
+            evt.dataTransfer.setData("text", this.innerHTML);
+            droppableIndex = parseInt(this.dataset.index);
+            console.log(this.innerHTML);
             return false;
         }, false);
         /////////////////////////////////////////////
         el.addEventListener("dragenter", function(evt){
-
+            //this.parentNode.classList.add("dragenter");
+            targetIndex = parseInt(this.dataset.index);
             return false;
         }, false);
         /////////////////////////////////////////////
@@ -21,25 +27,40 @@
             if(evt.preventDefault){
                 evt.preventDefault();
             }
+            //this.parentNode.classList.add("dragenter");
 
+            evt.dataTransfer.dropEffect = 'copy';
             return false;
         }, false);
         /////////////////////////////////////////////
         el.addEventListener("dragleave", function(evt){
-
+            // this.parentNode.classList.remove("dragenter");
         }, false);
         /////////////////////////////////////////////
         el.addEventListener("drop", function(evt){
             if(evt.preventDefault){
                 evt.preventDefault();
             }
-
+            if(dragSrcElm != this){
+                if(droppableIndex < targetIndex){ // sens descente
+                    for(var i = droppableIndex; i<targetIndex; i++){
+                        drags.item(i-1).innerHTML = drags.item(i).innerHTML;
+                    }
+                } else { // sens montée
+                    for(var i=droppableIndex;i>targetIndex ;i-- ){
+                        drags.item(i-1).innerHTML = drags.item(i-2).innerHTML;
+                    }
+                }
+                this.innerHTML = evt.dataTransfer.getData("text");
+            }
             return false;
         }, false);
         /////////////////////////////////////////////
         el.addEventListener("dragend", function(evt){
-
+            [].slice.call(drags).forEach(function(el){
+                // el.parentNode.classList.remove("dragenter");
+                // el.classList.remove("dragstart");
+            });
         }, false);
     });
-    console.log(drags);
 })(window, document);
